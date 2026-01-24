f7f2911ee517b625e3ab9ba72353f58f3e7c23c8
Kyle Smith <114899247+VoidWalker287@users.noreply.github.com>
2026-01-24 15:17:23 -0500

    Add discrete forward and reverse commands for testing (A/B)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -61,10 +61,15 @@ public class RobotContainer {
         RobotModeTriggers.disabled()
                 .whileTrue(drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        // joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        joystick.a().whileTrue(drivetrain.applyRequest(() -> drive.withVelocityX(MaxSpeed * 0.75)));
+        joystick.b().whileTrue(drivetrain.applyRequest(() -> drive.withVelocityX(-MaxSpeed * 0.75)));
+
+        /*
         joystick.b()
                 .whileTrue(drivetrain.applyRequest(
                         () -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+        */
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
